1f041f4ca490971bad7bf2d3445ef3a77c836127
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-05-25 16:02:25 +0800

    解构赋值解决方案

--- main.js
+++ main.js
@@ -19,12 +19,7 @@ window.jQuery = function (nodeOrSelector) {
     return nodes
 }
 
-window.jQuery.ajax = function (options) {
-    let url = options.url
-    let method = options.method
-    let body = options.body
-    let success = options.success
-    let fail = options.fail
+window.jQuery.ajax = function ({ url, method, body, success, fail }) {
 
     let request = new XMLHttpRequest()
     request.open(method, url)
